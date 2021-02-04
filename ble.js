e581de324dcaf1041564b87f4ac75c0e3024a959
Jakob Offersen <jakoboffersen@gmail.com>
2021-02-04 09:50:16 +0100

    add experimentalFeatures flag to browser-window. Still crashes on Big Sur

--- ble.js
+++ ble.js
@@ -2,5 +2,6 @@ function scan() {
     console.log("scanning...")
 
     navigator.bluetooth.requestDevice({ acceptAllDevices: true})
-    .then(device => console.log(device))
+    .then(device => console.log("Found a device!", device))
+    .catch(err => console.log("An error occurred...", err))
 }
